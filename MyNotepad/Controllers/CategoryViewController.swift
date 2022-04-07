3347c43a5497a1e0d4f615d94c846e7b5253bca7
Theordius <87307483+Theordius@users.noreply.github.com>
2022-04-07 20:46:44 +0200

    Add CategoryViewController definitions

--- MyNotepad/Controllers/CategoryViewController.swift
+++ MyNotepad/Controllers/CategoryViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import CoreData
 
-class CategoryViewController: UIViewController {
+
+class CategoryViewController: UITableViewController {
+    
+    var categories = [Category]()
+    
+    let contex = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadCategories()
+    }
+
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+    }
+    
+    //MARK: - Table Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! NotesListTableViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
     }
 
 
-}
 
